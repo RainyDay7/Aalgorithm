208843866d2ad28b30a8bff892f2e7a35b553c9c
RainyDay7 <1764682836@qq.com>
2020-03-29 23:50:15 +0800

    快速幂乘

--- src/FastPowerMultiplication.java
+++ src/FastPowerMultiplication.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+//O(logn)
 public class FastPowerMultiplication {
+
+    public static int pow(int x, int n){
+        //求x^n
+        int res = 1;
+        while(n>0){
+            if(n%2==1){
+                res*=x;
+            }
+            n/=2;
+            x*=x;
+        }
+        return res;
+    }
 }
