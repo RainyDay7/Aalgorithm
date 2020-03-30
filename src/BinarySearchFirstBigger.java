3baf54632460f72beb5e9f3678d53f6dbca98573
RainyDay7 <1764682836@qq.com>
2020-03-30 23:19:21 +0800

    查找第一个大于目标值的数

--- src/BinarySearchFirstBigger.java
+++ src/BinarySearchFirstBigger.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
 
+//查找第一个大于目标值的数
 public class BinarySearchFirstBigger {
+
+    public int binarySearch(int[]arr, int target){
+        int left = 0;
+        int right = arr.length-1;
+        while (left <= right){
+            int mid = left+(right-left)/2;
+            if(arr[mid]<=target){
+                mid = right+1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return  right;
+    }
+
 }
