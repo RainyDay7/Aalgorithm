efb05be9f9e174007887d303a25bc94494516e53
RainyDay7 <1764682836@qq.com>
2020-03-30 23:18:07 +0800

    链表从尾分组反转

--- src/ReverseListNodeFromTail.java
+++ src/ReverseListNodeFromTail.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+//把单链表进行一次逆序，逆序之后就能转化为从头部开始组起了，
+// 然后按照ReversrListNodeFromHead的解法，处理完之后，
+// 把结果再次逆序即搞定。两次逆序相当于没逆
 
 public class ReverseListNodeFromTail {
+
+    public  ListNode reverseLN(ListNode head, int k){
+        //先逆序一次
+        ReverseListNodeFromHead re = new ReverseListNodeFromHead();
+        head = re.reverseList(head);
+        head = re.reverseKGroup(head, k);
+        head = re.reverseList(head);
+        return head;
+    }
 }
