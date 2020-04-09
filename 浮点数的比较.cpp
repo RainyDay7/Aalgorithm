2f3c853759f4fa481b0f21cf9edcc842480eeb58
RainyDay7 <1764682836@qq.com>
2020-04-09 23:18:49 +0800

    eps = 1e-8

---
+++ 浮点数的比较.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+using namespace std;
+//浮点数的比较
+//eps取10^-8是一个很合适的数字
+const double eps = 1e-8;
+//为了比较方便，可以把比较操作携程宏定义的形式
+#define Equ(a,b) ((fabs(a-b))<eps) //==
+#define More(a,b) ((a-b)>eps)      //>
+#define Less(a,b) ((a-b)<(-eps))   //<
+#define MoreEqu(a,b) ((a-b)>(-eps)) //>=
+#define LessEqu(a,b) ((a-b)<(eps)) //>=
+
+
+//圆周率 不需要死记 cosΠ = -1 => pai = arccos(-1)
+const double Pi=acos(-1.0); 
+ 
+ int main(){
+ 	return 0;
+ }
